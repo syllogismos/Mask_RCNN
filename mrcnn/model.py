5d7d700a4f601d016280c66c80f920a874e19965
Anil Karaka <anilkaraka@live.in>
2019-05-08 23:08:06 +0530

    wandb callback

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2343,7 +2343,7 @@ class MaskRCNN():
                                         histogram_freq=0, write_graph=True, write_images=False),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
                                             verbose=0, save_weights_only=True),
-            wandb.keras.WandbCallback(),
+            wandb.keras.WandbCallback(save_weights_only=True, data_type='images'),
         ]
 
         # Add custom callbacks to the list
